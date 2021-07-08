0172935bf13548d3129eefe63b0981b077e1e4b7
sven-evenepoel <sven.evenepoel@student.odisee.be>
2021-07-08 10:53:36 +0200

    End of section 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+    console.log(response);  
+    setActivities(response.data);
+    })
+  },[])// comma and brackets makes sure that it won't go in a endless loop you need to define a array of depandency wich we will add later in the course
+
   return (
     <div className="App">
-      <header className="App-header">
+      <Header as='h2' icon='users'content='reactivities'/>
+  
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <List>
+          {activities.map((activity: any) =>(
+            <List.Item key={activity.id}>
+              {activity.title}
+            </List.Item>
+          ))}
+          </List>
     </div>
   );
 }
