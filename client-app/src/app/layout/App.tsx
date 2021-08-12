7709902b1edca4e00554db02bd340732ef63ff96
sven-evenepoel <sven.evenepoel@student.odisee.be>
2021-08-12 16:46:51 +0200

    folder structure

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
 
@@ -9,16 +7,15 @@ function App() {
 
   useEffect(() => {
     axios.get('http://localhost:5000/api/activities').then(response => {
-    console.log(response);  
     setActivities(response.data);
     })
   },[])// comma and brackets makes sure that it won't go in a endless loop you need to define a array of depandency wich we will add later in the course
 
   return (
-    <div className="App">
+    <div>
       <Header as='h2' icon='users'content='reactivities'/>
   
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <List>
           {activities.map((activity: any) =>(
             <List.Item key={activity.id}>
